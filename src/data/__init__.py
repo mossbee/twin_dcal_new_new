5391ac0076c2da51a37e92dd2e0aa3406d47df2f
mossbee <hoangnhatdb@gmail.com>
2025-07-22 08:55:19 +0700

    fix

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,5 +1,5 @@
-from .dataset import TwinDataset
+from .dataset import TwinDataset, TwinVerificationDataset
 from .transforms import get_train_transforms, get_val_transforms
 from .pair_sampler import TwinPairSampler
 
-__all__ = ['TwinDataset', 'get_train_transforms', 'get_val_transforms', 'TwinPairSampler'] 
+__all__ = ['TwinDataset', 'TwinVerificationDataset', 'get_train_transforms', 'get_val_transforms', 'TwinPairSampler'] 
